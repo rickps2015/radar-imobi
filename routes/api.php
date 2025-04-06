ea774536ebed00690a0d9899477ed6b6d1ef42cc
Ricardo Silva Parente <ricardosilvaparente@gmail.com>
2025-04-06 08:56:21 -0400

    feat: adicionando autorizacao de rotas de filtros apenas pra usuarios autenticados

--- routes/api.php
+++ routes/api.php
@@ -19,20 +19,19 @@ Route::get('/sale-modes', [PropertyController::class, 'getUniqueSaleModes']);
 Route::post('/scrape', [ScrapingController::class, 'postLeilaoData']);
 Route::get('/properties/top/{state}', [PropertyController::class, 'topDiscounted']);
 Route::get('/properties-count-by-state', [PropertyController::class, 'getPropertiesCountByState']);
+Route::get('/properties', [PropertyController::class, 'index']);
 Route::get('/ip-json', [PropertyController::class, 'getLocation']);
 
 Route::post('/send-reset-code', [NewPasswordController::class, 'sendResetCode']);
 Route::post('/verify-reset-code', [NewPasswordController::class, 'verifyResetCode']);
 Route::post('/check-reset-code', [NewPasswordController::class, 'checkResetCode']);
 
-Route::prefix('user-filters')->group(function() {
-    Route::post('/', [FilterController::class, 'store']); // Rota para criar um novo filtro
-    Route::get('/{userId}', [FilterController::class, 'index']); // Rota para listar filtros de um usuário
-    Route::delete('/{id}', [FilterController::class, 'destroy']);
-});
-
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/properties', [PropertyController::class, 'index']);
+    Route::prefix('user-filters')->group(function() {
+        Route::post('/', [FilterController::class, 'store']); // Rota para criar um novo filtro
+        Route::get('/{userId}', [FilterController::class, 'index']); // Rota para listar filtros de um usuário
+        Route::delete('/{id}', [FilterController::class, 'destroy']);
+    });
 });
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
